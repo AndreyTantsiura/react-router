18667941c3db9a64bc37991b89b662133ebf39ee
Andrew <https://github.com/AndreyTantsiura>
2021-08-22 12:14:47 +0300

    added checkings and adding to localstorage

--- src/components/SignUpComp.js
+++ src/components/SignUpComp.js
@@ -1,126 +1,81 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import padlock from "../img/padlock.png";
-import styled from "styled-components";
 import { Switch, Route, Link } from "react-router-dom";
 import SignInComp from "./SignUpComp";
-
-const Wrapper = styled.div`
-  width: 350px;
-  height: 500px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 10%;
-  display: flex;
-  justify-content: center;
-  background-color: #121212;
-`;
-
-const Inner = styled.form`
-  width: 270px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-family: "Roboto", sans-serif;
-  justify-content: center;
-  overflow: hidden;
-`;
-
-const Icon = styled.div`
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: pink;
-  margin-bottom: 10px;
-`;
-
-const SignIn = styled.div`
-  margin-bottom: 20px;
-  color: #929292;
-`;
-
-const InputNames = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 8px 0;
-`;
-
-const FirstName = styled.input`
-  width: 45%;
-  height: 30px;
-  background: initial;
-  border: 1px solid #424242;
-  border-radius: 3px;
-  outline: none;
-  color: #929292;
-`;
-
-const LastName = styled.input`
-  width: 45%;
-  height: 30px;
-  background: initial;
-  border: 1px solid #424242;
-  border-radius: 3px;
-  outline: none;
-  color: #929292;
-`;
-
-const InputsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  height: 30px;
-  background: initial;
-  border: 1px solid #424242;
-  border-radius: 3px;
-  outline: none;
-  color: #929292;
-  margin: 8px 0;
-`;
-
-const CheckboxWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-
-const Checkbox = styled.input`
-  margin: 0 5px 0 0;
-  font-size: 12px;
-`;
-
-const Button = styled.button`
-  width: 100%;
-  border-radius: 3px;
-  border: none;
-  background-color: #a1d1f8;
-  font-size: 9px;
-  padding: 9px;
-  margin-bottom: 15px;
-  cursor: pointer;
-  font-weight: bold;
-`;
-
-const Links = styled.div`
-  width: 100%;
-  font-size: 10px;
-  color: #a1d1f8;
-  margin-bottom: 40px;
-  text-align: end;
-`;
-
-const Span = styled.span`
-  font-size: 11px;
-  color: #929292;
-`;
+import {
+  Wrapper,
+  Inner,
+  Icon,
+  SignIn,
+  InputsWrapper,
+  Input,
+  CheckboxWrapper,
+  Checkbox,
+  Button,
+  Links,
+  Span,
+  InputNames,
+  FirstName,
+  LastName,
+} from "./styledFormsComponents";
 
 export default function SignUpComp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLAstNAme] = useState("");
+
+  const fisrtNAmeHandler = (e) => {
+    const nameCheck = new RegExp(/[A-Za-z]{3,}/);
+    setFirstName(e.target.value);
+
+    if (e.target.value.match(nameCheck)) {
+      e.target.style.borderColor = "green";
+      setFirstName(e.target.value);
+    } else {
+      e.target.style.borderColor = "red";
+    }
+    
+  };
+
+  const lastNameHandler = (e) => {
+    const nameCheck = new RegExp(/[A-Za-z]{3,}/);
+    setLAstNAme(e.target.value);
+
+    if (e.target.value.match(nameCheck)) {
+      e.target.style.borderColor = "green";
+      setLAstNAme(e.target.value);
+    } else {
+      e.target.style.borderColor = "red";
+    }
+  };
+
+  const emailHandler = (e) => {
+    const emailCheck = new RegExp(/^\S{3,}@\S{2,}\.\D{2,}/);
+    setEmail(e.target.value);
+
+    if (e.target.value.match(emailCheck)) {
+      e.target.style.borderColor = "green";
+    } else {
+      e.target.style.borderColor = "red";
+    }
+  };
+
+  const passwordHandler = (e) => {
+    const passCheck = new RegExp(/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{8,}/);
+    setPassword(e.target.value);
+
+    if (e.target.value.match(passCheck)) {
+      e.target.style.borderColor = "green";
+    } else {
+      e.target.style.borderColor = "red";
+    }
+  };
+const user = {firstName, lastName, password, email}
+  const rememberDataUser = () => {
+    localStorage.setItem("user", JSON.stringify(user))
+  }
+
   return (
     <Wrapper>
       <Inner>
@@ -129,15 +84,29 @@ export default function SignUpComp() {
         </Icon>
         <SignIn>Sign up</SignIn>
         <InputNames>
-          <FirstName placeholder="First Name*"></FirstName>
-          <LastName placeholder="Last Name*"></LastName>
+          <FirstName
+            onChange={(e) => fisrtNAmeHandler(e)}
+            placeholder="First Name*"
+          ></FirstName>
+          <LastName
+            onChange={(e) => lastNameHandler(e)}
+            placeholder="Last Name*"
+          ></LastName>
         </InputNames>
         <InputsWrapper>
-          <Input placeholder="Email Adress *"></Input>
-          <Input placeholder="Password *"></Input>
+          <Input
+            value={email}
+            onChange={(e) => emailHandler(e)}
+            placeholder="Email Adress *"
+          ></Input>
+          <Input
+            value={password}
+            onChange={(e) => passwordHandler(e)}
+            placeholder="Password *"
+          ></Input>
         </InputsWrapper>
         <CheckboxWrapper>
-          <Checkbox type="checkbox" />
+          <Checkbox type="checkbox" onClick={rememberDataUser}/>
           <Span>
             I want to receive inspiration, marketing promoyions and updates via
             email
@@ -145,18 +114,7 @@ export default function SignUpComp() {
         </CheckboxWrapper>
         <Button>SIGN UP</Button>
         <Links>
-          <Link
-            to="/sign_in"
-            style={{
-              textDecoration: "none",
-              color: "#a1d1f8",
-              border: "none",
-              padding: 0,
-              margin: 0,
-              cursor: "pointer",
-            }}
-            class="hover-item"
-          >
+          <Link to="/sign_in" className="hover-item">
             Aready have an account? Sign in
           </Link>
         </Links>
---
+++ src/components/styledFormsComponents.js
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 350px;
+  height: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10%;
+  display: flex;
+  justify-content: center;
+  background-color: #121212;
+`;
+
+export const Inner = styled.form`
+  width: 270px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto", sans-serif;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+export const Icon = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: pink;
+  margin-bottom: 10px;
+`;
+
+export const SignIn = styled.div`
+  margin-bottom: 10px;
+  color: #929292;
+`;
+
+export const InputsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const Input = styled.input`
+  height: 30px;
+  background: initial;
+  border: 1px solid grey;
+  border-radius: 3px;
+  outline: none;
+  color: #929292;
+  margin: 8px 0;
+`;
+
+export const CheckboxWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const Checkbox = styled.input`
+  margin: 0 5px 0 0;
+  font-size: 12px;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  border-radius: 3px;
+  border: none;
+  background-color: #a1d1f8;
+  font-size: 9px;
+  padding: 9px;
+  margin-bottom: 15px;
+  cursor: pointer;
+  font-weight: bold;
+`;
+
+export const Links = styled.div`
+  width: 100%;
+  justify-content: space-between;
+  display: flex;
+  font-size: 10px;
+  color: #a1d1f8;
+  margin-bottom: 40px;
+`;
+
+export const Span = styled.span`
+  font-size: 11px;
+  color: #929292;
+`;
+
+export const InputNames = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 8px 0;
+`;
+
+export const FirstName = styled.input`
+  width: 45%;
+  height: 30px;
+  background: initial;
+  border: 1px solid grey;
+  border-radius: 3px;
+  outline: none;
+  color: #929292;
+`;
+
+export const LastName = styled.input`
+  width: 45%;
+  height: 30px;
+  background: initial;
+  border: 1px solid grey;
+  border-radius: 3px;
+  outline: none;
+  color: #929292;
+`;
+
